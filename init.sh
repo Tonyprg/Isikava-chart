54a6c9a5b1a4b1101f735f5a845736fbf7cbb950
Tonyprg <anatolij_ivanov_1999@list.ru>
2023-06-29 10:34:17 +0500

    add_init

---
+++ init.sh
@@ -0,0 +1,7 @@
+sudo apt upgrade python3 -y
+sudo apt upgrade pip -y
+sudo apt upgrade python-tk -y
+sudo apt upgrade python-pil -y
+pip install pyscreenshot -y
+pip install tkcalendar -y
+pip install numpy -y
